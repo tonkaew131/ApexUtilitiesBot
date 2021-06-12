f6cf996ef3d17de9179de5d3e988e5166bcb5285
tonkaew131 <nongtonkaew@gmail.com>
2021-06-12 18:34:37 +0700

    Implement API funciton

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-require('dotenv').config()
+require('dotenv').config();
+
+const APIUtils = require('./lib/api');
+
+const globalConfig = {
+    prefix: 'au!'
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+client.on('message', async message => {
+    const args = message.content.slice(globalConfig['prefix'].length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command == 'map') {
+        let mapData = await APIUtils.getMapRotationAPI();
+
+        console.log(mapData);
+        return;
     }
 });
 
